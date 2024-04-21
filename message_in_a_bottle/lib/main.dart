40132907ddf63f3d2e347950fbe605969b061ffc
FreeTechSupport <arkanshaheen30@gmail.com>
2024-04-21 01:39:27 -0400

    Moved BottleLocationsProvider declaration to main

--- message_in_a_bottle/lib/main.dart
+++ message_in_a_bottle/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:message_in_a_bottle/pages/login.dart';
+import 'package:message_in_a_bottle/providers/bottle_locations_provider.dart';
 import 'package:message_in_a_bottle/providers/curr_user_location.dart';
 import 'package:message_in_a_bottle/pages/map_page.dart';
 import 'package:provider/provider.dart';
@@ -18,9 +19,7 @@ void main() async {
   Widget homePage = const LoginPage();
 
   // checks if the user is logged in; if not, they are sent to the login page
-  FirebaseAuth.instance
-  .authStateChanges()
-  .listen((User? user) {
+  FirebaseAuth.instance.authStateChanges().listen((User? user) {
     print(user.toString());
     if (user == null) {
       print('User is currently signed out!');
@@ -29,13 +28,13 @@ void main() async {
       print('User is signed in!');
       homePage = MapPage(user: user);
     }
-  }
-  );
+  });
 
   runApp(
     MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => CurrentUserLocationProvider()),
+        ChangeNotifierProvider(create: (_) => BottleLocationsProvider()),
       ],
       child: MaterialApp(
         home: homePage,
